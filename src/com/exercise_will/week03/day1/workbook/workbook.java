213b9e00512b4b51edbbf2b2e913bc8396537657
William Burdett <williamburdett1995@gmail.com>
2022-01-31 20:55:36 +0000

    Delete mapping added

--- src/com/exercise_will/week03/day1/workbook/workbook.java
+++ src/com/exercise_will/week03/day1/workbook/workbook.java
@@ -1,5 +1,7 @@
 package com.exercise_will.week03.day1.workbook;
 
+import java.util.Arrays;
+
 public class workbook {
     // static property, class or field means it belongs to the class itself and not the objects
     // Person public class {
@@ -58,7 +60,7 @@ public class workbook {
         );
 
         // Person who owns the Tesla and BMW
-        Car[] nasirCars = {car};
+        Car[] nasirCars = {car2, car3};
         Person nasir = new Person(
                 "Nasir",
                 22,
@@ -75,7 +77,7 @@ public class workbook {
                 null
         );
 
-        //
+        // the Garage
         Car[] cars2 = {car2};
         Garage garage = new Garage(
                 michael,
@@ -89,56 +91,110 @@ public class workbook {
         GarageService garageService = new GarageService();
 
         // add car
+        System.out.println("Add car should add Honda:");
         garageService.addCar(garage, car);
-        System.out.println(garage);
+        System.out.println(Arrays.toString(garage.getCars()));
+
+        System.out.println("------------------------");
 
         // count cars
+        System.out.println("Count cars should return 1:");
         System.out.println(garageService.carCount(garage));
 
+        System.out.println("------------------------");
+
         // free spots
+        System.out.println("Spots free should return 2:");
         System.out.println(garageService.freeSpots(garage));
 
+        System.out.println("------------------------");
+
         // remove car
-       garageService.removeCar(garage, car);
-        System.out.println(garage);
+        System.out.println("Garage should be empty:");
+        garageService.removeCar(garage, car);
+        System.out.println(Arrays.toString(garage.getCars()));
+
+        System.out.println("------------------------");
+
 
         // open and close garage
+        System.out.println("Close should cause false:");
         garageService.closeGarage(garage);
-        System.out.println(garage);
+        System.out.println(garage.isOpen());
+
+        System.out.println("------------------------");
+
+        System.out.println("Open should cause true:");
         garageService.openGarage(garage);
-        System.out.println(garage);
+        System.out.println(garage.isOpen());
 
         // is full?
+        System.out.println("Full should return false:");
+        System.out.println(Arrays.toString(garage.getCars()));
         System.out.println(garageService.isFull(garage));
 
+        System.out.println("------------------------");
+
         // adding cars
+        System.out.println("Add should add Honda:");
         garageService.addCar(garage, car);
+        System.out.println(Arrays.toString(garage.getCars()));
+
+        System.out.println("Add should add BMW:");
         garageService.addCar(garage, car2);
+        System.out.println(Arrays.toString(garage.getCars()));
+
+        System.out.println("------------------------");
 
         // filter electric cars
+        System.out.println("Should return electrics:");
         System.out.println(garageService.filterElectric(garage));
+
+        System.out.println("------------------------");
+
         // filter non-electric cars
+        System.out.println("Should return non-electrics:");
         System.out.println(garageService.filterNonElectric(garage));
+
+        System.out.println("------------------------");
 
         // clean garage
        garageService.cleanGarage(garage);
+
+        System.out.println("------------------------");
 
         // writing stored cars data to file
         GarageDAO garageDAO = new GarageDAO();
         garageDAO.postMapping(garage);
 
+
         // reading garage
+        System.out.println("What's in the garage? :");
         garageDAO.readGarage(garage);
-
-
         System.out.println("------------------------");
 
         // findByManufacturer
+        System.out.println("Should find the BMW and return info:");
         System.out.println(garageDAO.findByManufacturer(garage, "bmw"));
 
         System.out.println("------------------------");
 
-        //findByRegNum
+        // findByRegNum
+        System.out.println("Should return car not found:");
         System.out.println(garageDAO.findByRegNum(garage, "0905"));
+        System.out.println("Should find car reg and return Honda info::");
+        System.out.println(garageDAO.findByRegNum(garage, "1234"));
+
+        // deleteMapping
+        System.out.println("------------------------");
+        System.out.println("Garage before deleting car:");
+        System.out.println(Arrays.toString(garage.getCars()));
+
+        System.out.println("------------------------");
+
+        System.out.println("Garage after deleting Honda:");
+        garageDAO.deleteByRegNum(garage, "1234");
+        System.out.println(Arrays.toString(garage.getCars()));
+
     }
 }
